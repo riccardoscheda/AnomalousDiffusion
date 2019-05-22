a4e530bf824dbf02e8aba1a40d71e2fdc25db49f
riccardo.scheda <riccardo.scheda@gmail.com>
2019-05-22 13:41:58 +0200

    added test for adaptive_contrast enhancement function

--- test_classification.py
+++ test_classification.py
@@ -1,9 +1,21 @@
 import os
-import classification
-
+import classification as cl
+import numpy as np
 
 def test_create_set():
     """
-    Verifies if the number of images in the set is correct
+    Tests if the number of images in the set is correct
     """
-    assert len(os.listdir("images/")) == classification.n_images
+    assert len(os.listdir("images/")) == cl.n_images
+
+def test_create_modified_images():
+    """
+    Testa if the number of images in the set is correct
+    """
+    assert len(os.listdir("modified_images/")) == cl.n_images
+
+def test_adaptive_contrast_enhancement():
+    """
+    Tests if the number of images in the set is correct
+    """
+    assert isinstance(cl.adaptive_contrast_enhancement(cl.image[0],(10,10)), np.ndarray) == True
