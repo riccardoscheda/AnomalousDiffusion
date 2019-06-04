83bb33071540b003d98684d6e5e3990f01a7ce18
riccardo.scheda <riccardo.scheda@gmail.com>
2019-06-04 13:01:24 +0200

    now label can label all the images in the current directory, and saved the labelled images in the previous diretcory 'labelled_images'

--- andif/andif/__main__.py
+++ andif/andif/__main__.py
@@ -22,7 +22,7 @@ import fronts as fr
 ######################################################
 
 path = "."
-outfolder = "labelled_images"
+outfolder = "../labelled_images"
 if not os.path.exists(outfolder):
     os.makedirs(outfolder)
 
@@ -40,22 +40,39 @@ class AnomalousDiffusion(cli.Application):
 @AnomalousDiffusion.subcommand("label")
 class Label(cli.Application):
     "Saves the labelled image using pca and K-means algorithms"
+    #all = cli.Flag(["all", "every image"], help = "If given, I will label all the images in the current directory")
     def main(self, value : str):
-        if (value not in list(os.listdir(path))):
-            print(colors.red|"this image does not exists")
+        if(value == "all"):
+            cont = 0
+            for value in list(os.listdir(".")):
+                test_image =  cv2.imread(value)
+                im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+                print("image taken " +str(cont)+"/"+str(len(os.listdir("."))))
+                #print("i'm doing PCA on the LBP image")
+                pca = cl.Principal_components_analysis(im_gray)
+                #print("PCA finished")
+                #print("Now i'm using K-means to classify the subimages")
+                labelled_image = cl.classification(pca)
+                #print("K-means finished")
+                plt.imsave("../labelled_images/labelled_"+str(cont)+".png",labelled_image)
+
+            print(colors.green|"Saved the labelled images in dir 'labelled_images/'")
 
         else:
-            test_image =  cv2.imread(value)
-            im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
-            print("image taken")
-            print("i'm doing PCA on the LBP image")
-            pca = cl.Principal_components_analysis(im_gray)
-            print("PCA finished")
-            print("Now i'm using K-means to classify the subimages")
-            labelled_image = cl.classification(pca)
-            print("K-means finished")
-            plt.imsave("../labelled_images/labelled_"+value,labelled_image)
-            print(colors.green|"Saved the labelled image in dir 'labelled_images/'")
+            if(value not in list(os.listdir(path))):
+                print(colors.red|"this image does not exists")
+            else:
+                test_image =  cv2.imread(value)
+                im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+                print("image taken")
+                print("i'm doing PCA on the LBP image")
+                pca = cl.Principal_components_analysis(im_gray)
+                print("PCA finished")
+                print("Now i'm using K-means to classify the subimages")
+                labelled_image = cl.classification(pca)
+                print("K-means finished")
+                plt.imsave("../labelled_images/labelled_"+value,labelled_image)
+                print(colors.green|"Saved the labelled image in dir 'labelled_images/'")
 
 
 if __name__ == "__main__":
