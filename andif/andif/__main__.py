0c70dc61ea0b4106d2688ac8ccf1e8ee4e16bca0
riccardo.scheda <riccardo.scheda@gmail.com>
2019-06-04 16:51:23 +0200

    added the flag --all to the command label to label all the images in the current direcory. added function fronts but not yet implemented

--- andif/andif/__main__.py
+++ andif/andif/__main__.py
@@ -4,7 +4,7 @@ from plumbum import local
 from PIL import Image as imreader
 from plumbum.cli.image import Image as imdisplay
 from plumbum.cli.terminal import Progress
-import time
+
 ############################################
 import cv2
 import matplotlib.pyplot as plt
@@ -40,24 +40,27 @@ class AnomalousDiffusion(cli.Application):
 @AnomalousDiffusion.subcommand("label")
 class Label(cli.Application):
     "Saves the labelled image using pca and K-means algorithms"
-    #all = cli.Flag(["all", "every image"], help = "If given, I will label all the images in the current directory")
-    def main(self, value : str):
-        if(value == "all"):
-            cont = 0
-            for value in list(os.listdir(".")):
-                test_image =  cv2.imread(value)
-                im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
-                print("image taken " +str(cont)+"/"+str(len(os.listdir("."))))
-                #print("i'm doing PCA on the LBP image")
-                pca = cl.Principal_components_analysis(im_gray)
-                #print("PCA finished")
-                #print("Now i'm using K-means to classify the subimages")
-                labelled_image = cl.classification(pca)
-                #print("K-means finished")
-                plt.imsave("../labelled_images/labelled_"+str(cont)+".png",labelled_image)
-
-            print(colors.green|"Saved the labelled images in dir 'labelled_images/'")
-
+    all = cli.Flag(["all", "every image"], help = "If given, I will label all the images in the current directory")
+    def main(self, value : str = ""):
+        if(value == ""):
+            if (self.all):
+                cont = 0
+                for value in list(os.listdir(".")):
+                    if str(value).endswith(".png"):
+                        test_image =  cv2.imread(value)
+                        im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+                        print("analyzing image " +str(cont+1)+"/"+str(len(os.listdir("."))))
+                        #print("i'm doing PCA on the LBP image")
+                        pca = cl.Principal_components_analysis(im_gray)
+                        #print("PCA finished")
+                        #print("Now i'm using K-means to classify the subimages")
+                        labelled_image = cl.classification(pca)
+                        #print("K-means finished")
+                        plt.imsave("../labelled_images/labelled_"+str(cont)+".png",labelled_image)
+                        cont = cont + 1
+                print(colors.green|"Saved the labelled images in dir 'labelled_images/'")
+            else:
+                print(colors.red|"image not given")
         else:
             if(value not in list(os.listdir(path))):
                 print(colors.red|"this image does not exists")
@@ -73,6 +76,12 @@ class Label(cli.Application):
                 print("K-means finished")
                 plt.imsave("../labelled_images/labelled_"+value,labelled_image)
                 print(colors.green|"Saved the labelled image in dir 'labelled_images/'")
+
+@AnomalousDiffusion.subcommand("fronts")
+class Frontsl(cli.Application):
+    "Tracks the borders of the cells and saves the coordinates"
+    def main(self):
+        print("not yet implemented")
 
 
 if __name__ == "__main__":
