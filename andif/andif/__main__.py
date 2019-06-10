78200f40d1d7b2fad1e288099a3831b1e74da690
riccardo.scheda <riccardo.scheda@gmail.com>
2019-06-10 09:26:30 +0200

    corrected comments and definitions

--- andif/andif/__main__.py
+++ andif/andif/__main__.py
@@ -1,14 +1,14 @@
 from plumbum import cli, colors
 import os
 from plumbum import local
-from PIL import Image as imreader
-from plumbum.cli.image import Image as imdisplay
-from plumbum.cli.terminal import Progress
+#from PIL import Image as imreader
+#from plumbum.cli.image import Image as imdisplay
+#from plumbum.cli.terminal import Progress
 
 ############################################
 import cv2
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
 import os
@@ -36,7 +36,7 @@ class AnomalousDiffusion(cli.Application):
 
 @AnomalousDiffusion.subcommand("label")
 class Label(cli.Application):
-    "Saves the labelled image using pca and K-means algorithms"
+    "Saves the binary image using pca and K-means algorithms"
     all = cli.Flag(["all", "every image"], help = "If given, I will label all the images in the current directory")
     def main(self, value : str = ""):
         if not os.path.exists("labelled_images"):
@@ -50,15 +50,11 @@ class Label(cli.Application):
                         test_image =  cv2.imread(value)
                         im_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
                         print("analyzing image " +str(cont+1)+"/"+str(len(os.listdir("."))))
-                        #print("i'm doing PCA on the LBP image")
                         pca = cl.Principal_components_analysis(im_gray)
-                        #print("PCA finished")
-                        #print("Now i'm using K-means to classify the subimages")
                         labelled_image = cl.classification(im_gray, pca)
-                        #print("K-means finished")
                         plt.imsave("labelled_images/labelled_"+value,labelled_image)
                         cont = cont + 1
-                print(colors.green|"Saved the labelled images in dir 'labelled_images/'")
+                print(colors.green|"Saved the binary images in dir 'labelled_images/'")
             else:
                 print(colors.red|"image not given")
         else:
@@ -79,7 +75,7 @@ class Label(cli.Application):
 
 @AnomalousDiffusion.subcommand("fronts")
 class Fronts(cli.Application):
-    "Tracks the borders of the cells and saves the coordinates"
+    "Tracks the longest borders in the images and saves the coordinates in a txt file"
     all = cli.Flag(["all", "every image"], help = "If given, I will save the fronts of all the images in the current directory")
     def main(self, value : str = ""):
         if not os.path.exists("fronts"):
@@ -107,24 +103,24 @@ class Fronts(cli.Application):
 
 @AnomalousDiffusion.subcommand("divide")
 class Divide(cli.Application):
-    "Divides the front in two piecies, one left and one right"
+    "Divides the front in two piecies, one left and one right and save them in two txt files"
     all = cli.Flag(["all", "every text file"], help = "If given, I will save the dx and sx fronts of all the images in the current directory")
     def main(self, value : str = ""):
-        if not os.path.exists("fronts"):
-            os.makedirs("fronts")
+        if not os.path.exists("divided_fronts"):
+            os.makedirs("divided_fronts")
 
         if(value == ""):
             if (self.all):
                 cont = 0
                 for value in list(os.listdir(".")):
                     if str(value).endswith(".txt"):
-                        df =pd.DataFrame(pd.read_csv(value , delimiter=' '))
+                        df = pd.DataFrame(pd.read_csv(value , delimiter=' '))
                         df.columns = ["x","y"]
                         sx, dx = fr.divide(df)
-                        np.savetxt("fronts/dx"+ value , dx.values, fmt='%d')
-                        np.savetxt("fronts/sx"+ value , sx.values, fmt='%d')
+                        np.savetxt("divided_fronts/dx"+ value , dx.values, fmt='%d')
+                        np.savetxt("divided_fronts/sx"+ value , sx.values, fmt='%d')
                         cont = cont + 1
-                print(colors.green|"Divided the fronts of the images in dir 'fronts/'")
+                print(colors.green|"Divided the fronts of the images in dir 'divided_fronts/'")
             else:
                 print(colors.red|"file not given")
         else:
@@ -135,9 +131,9 @@ class Divide(cli.Application):
                 df = pd.DataFrame(pd.read_csv(value , delimiter=' '))
                 df.columns = ["x","y"]
                 sx, dx = fr.divide(df)
-                np.savetxt("fronts/dx"+ value , dx.values, fmt='%d')
-                np.savetxt("fronts/sx"+ value , sx.values, fmt='%d')
-                print(colors.green|"Divided the fronts  and saved in dir 'fronts/'")
+                np.savetxt("divided_fronts/dx"+ value , dx.values, fmt='%d')
+                np.savetxt("divided_fronts/sx"+ value , sx.values, fmt='%d')
+                print(colors.green|"Divided the fronts and saved in dir 'divided_fronts/'")
 
 
 
